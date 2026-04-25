ac2effbc66dc3b2f42cbf3ec55248a2186a4474a
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-24 20:25:13 -0600

    main.ts refactor

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
 
+  //Configuración de CORS (Vital para que el frontend se conecte
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true, // Transforma los payloads a los tipos del DTO
     }),
   );
 
@@ -23,6 +27,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  //Solo una vez el app.listen y con '0.0.0.0'
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`API ejecutándose en el puerto: ${port}`);
+  console.log(`Documentación disponible en: /api/docs`);
 }
 bootstrap();
