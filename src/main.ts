9b437d9d2eda9e56c98424975deb52d697515df0
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-04-24 19:41:53 -0600

    Add config Main

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
@@ -23,6 +24,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`API ejecutándose en el puerto: ${port}`);
+  console.log(`Documentación disponible en: /api/docs`);
 }
 bootstrap();
